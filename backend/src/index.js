e06cb08663228cabf312db4c33f233cf929b6312
Kenneth E. Puliche C. <kennethdevpc@gmail.com>
2025-01-24 20:23:41 -0500

    el cambio de front con frontend porque estaba generando errores

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,7 +26,7 @@ app.use('/api', api);
 
 //----------"configuracion para produccion"
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist'))); //---entonces aqui usa los archivos estaticos
+  app.use(express.static(path.join(__dirname, '../front/dist'))); //---entonces aqui usa los archivos estaticos
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../front', 'dist', 'index.html'));
